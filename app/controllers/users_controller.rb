aa1525c7967a2566b2f4e8260c067e51f51799d4
PedroHamzehT <pedrohamzeh@gmail.com>
2022-11-24 08:36:09 -0300

    add event types for the user model and "me" page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def me
+    @event_types = current_user.event_types
   end
 end
